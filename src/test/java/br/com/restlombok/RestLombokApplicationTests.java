1d3f3166ecff1ac8ce9111d6eb651a3abc30a11a
gustavoaquino <guuaquino@hotmail.com>
2021-04-11 13:32:24 -0300

    Iniciando aplicação

---
+++ src/test/java/br/com/restlombok/RestLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
